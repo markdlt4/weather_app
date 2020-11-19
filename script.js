2e9b24b2dd280d6c22a530d71737fd9a303d5f59
markt <markdlt4@gmail.com>
2020-11-19 14:02:38 -0600

    edited api components

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ function citySearch() {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-
+        $("#name").append(response.name)
         // Printing the entire object to console
         console.log(response);
+        var lat=response.coord.lat
+        var lon=response.coord.lon
+        http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
     })
 }
 $("#city").on("click", citySearch)
