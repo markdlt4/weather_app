b9cb004559d2e08ca3eeccad39c4f1955277f1e6
markt <markdlt4@gmail.com>
2020-11-19 13:38:35 -0600

    added api key

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function citySearch(){
+
+function citySearch() {
     console.log("I worked")
+    var city = $("#cityInput").val()
+    console.log(city)
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=93c464dafc5040907d7028517a022299";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        // Printing the entire object to console
+        console.log(response);
+    })
 }
-$("#city").on("click",citySearch)
+$("#city").on("click", citySearch)
