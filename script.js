2e4db27ba3848eba3c59a0113afb5737a03e8c66
markt <markdlt4@gmail.com>
2020-11-21 18:15:05 -0600

    add functions to js

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
+$(document).ready(function () {
+    $('#searchBtn').click(function () {
 
 function citySearch() {
-    console.log("I worked")
+    //console.log("I worked")
     var city = $("#cityInput").val()
-    console.log(city)
+    //console.log(city)
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=93c464dafc5040907d7028517a022299";
-    $.ajax({
+    var lat=response.coord.lat
+    var lon=response.coord.lon$.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         $("#name").append(response.name)
         // Printing the entire object to console
-        console.log(response);
-        var lat=response.coord.lat
-        var lon=response.coord.lon
-        http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
+        //console.log(response);
+
+        response.name;
+        response.main.temp;
+        response.main.humidity;
+        response.wind.speed;
+        $('#cityName').text(
+            response.name + ' (' + new Date().toLocaleDateString() + ')'
+        );
+        $('#cityName').append(
+        );
+        $('currentTemp')
+            .text('Temperature: ${respone.main.temp}' + '° F')
+            .addClass('currentWeather');
+        $('humidity')
+            .text('Humidity: ${response.main.humidity}' + '%')
+            .addClass('currentWeather');
+            
+        
     })
 }
 $("#city").on("click", citySearch)
+//http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
+//http://openweathermap.org/img/wn/${response.weather[0].icon}.png
